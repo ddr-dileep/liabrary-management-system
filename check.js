fb3dbb9f928e44f34d53c2f3881a00ccba3321ee
Dileep <ddr.dileepraloti@gmail.com>
2024-09-23 17:57:09 +0530

    feat : send sms

---
+++ check.js
@@ -0,0 +1,21 @@
+// const twilio = require("twilio");
+
+// // Create a Twilio client
+// const client = twilio(accountSid, authToken);
+
+// // Send an SMS
+// const sendSms = async () => {
+//   try {
+//     const message = await client.messages.create({
+//       body: "Hello! This is a test message from Node.js using Twilio.",
+//       from: "+13235534243", // Your Twilio phone number
+//       to: "+917------", // Recipient's phone number
+//     });
+
+//     console.log("Message sent successfully:", message.sid);
+//   } catch (error) {
+//     console.error("Error sending SMS:", error);
+//   }
+// };
+
+// sendSms();
